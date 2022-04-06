73dc81a47bcc5f2d77eda0712f59af06e19257a2
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-04-06 15:37:06 +0200

    lzw decode works

--- src/lzw.rs
+++ src/lzw.rs
@@ -74,6 +74,57 @@ impl LzwDictionary {
     }
 }
 
+pub struct LzwDecodeDictionary {
+    dict: Vec<Vec<u8>>,
+    last_word: Option<Vec<u8>>,
+}
+
+impl LzwDecodeDictionary {
+    pub fn new() -> LzwDecodeDictionary {
+        let dict = (0..128).map(|n| vec![n]).collect();
+
+        LzwDecodeDictionary {
+            dict,
+            last_word: None,
+        }
+    }
+
+    pub fn get_next_word<I>(&mut self, codes: &mut I) -> Option<Vec<u8>>
+    where
+        I: Iterator<Item = usize>,
+    {
+        let code = match codes.next() {
+            Some(c) => c,
+            None => return None,
+        };
+
+        match self.find_word(code) {
+            Some(w) => {
+                if let Some(mut last_word) = self.last_word.take() {
+                    last_word.push(w[0]);
+                    self.dict.push(last_word);
+                }
+
+                self.last_word = Some(w.clone());
+
+                Some(w)
+            }
+            None => {
+                let mut last_word = self.last_word.take().expect("has to be some");
+                last_word.push(last_word[0]);
+
+                self.last_word = Some(last_word.clone());
+
+                Some(last_word)
+            }
+        }
+    }
+
+    fn find_word(&self, code: usize) -> Option<Vec<u8>> {
+        self.dict.get(code).map(|v| v.to_vec())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -110,6 +161,24 @@ mod tests {
             codes.push(c);
         }
 
-        assert_eq!(vec![0], codes);
+        assert_eq!(vec![0, 1, 128, 130, 129, 1], codes);
+    }
+
+    #[test]
+    fn lzw_decode_works() {
+        let mut lzw_decode_dict = LzwDecodeDictionary::new();
+        let codes = [0, 1, 128, 130, 129, 1];
+        let mut codes_iter = codes.into_iter();
+
+        let mut words = vec![];
+
+        while let Some(w) = lzw_decode_dict.get_next_word(&mut codes_iter) {
+            words.push(w);
+        }
+
+        assert_eq!(
+            vec![0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            words.into_iter().flatten().collect::<Vec<_>>()
+        );
     }
 }

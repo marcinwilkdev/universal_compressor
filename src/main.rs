057540cd88bf3edd0d438687e0ba8d964bba2e50
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-04-07 13:17:02 +0200

    adjust main demo to new project structure

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use universal_compressor::{EliasOmegaDecoder, EliasOmegaEncoder};
+
 fn main() {
     let data = std::fs::read("txt_file").expect("file doesnt exist");
 
-    let encoded = universal_compressor::encode(&data);
+    let encoded = universal_compressor::encode::<EliasOmegaEncoder>(&data);
 
-    let decoded = universal_compressor::decode(&encoded);
+    let decoded = universal_compressor::decode::<EliasOmegaDecoder>(&encoded);
 
     let text = String::from_utf8_lossy(&decoded);
 

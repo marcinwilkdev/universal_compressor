fd76a22d0aaaaf2189a3ca6780ceda993a93e731
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-04-07 14:58:19 +0200

    adjust demo to use elias delta encoding

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use universal_compressor::{EliasOmegaDecoder, EliasOmegaEncoder};
+// use universal_compressor::{EliasOmegaDecoder, EliasOmegaEncoder};
+use universal_compressor::{EliasDeltaEncoder, EliasDeltaDecoder};
 
 fn main() {
     let data = std::fs::read("txt_file").expect("file doesnt exist");
 
-    let encoded = universal_compressor::encode::<EliasOmegaEncoder>(&data);
+    let encoded = universal_compressor::encode::<EliasDeltaEncoder>(&data);
 
-    let decoded = universal_compressor::decode::<EliasOmegaDecoder>(&encoded);
+    let decoded = universal_compressor::decode::<EliasDeltaDecoder>(&encoded);
 
     let text = String::from_utf8_lossy(&decoded);
 
